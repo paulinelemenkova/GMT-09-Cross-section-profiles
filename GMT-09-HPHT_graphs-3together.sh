e397a7acf8f5cc52d7375e7a140d7a5f5ee7faf5
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2020-07-03 12:04:09 +0300

    Add files via upload

--- GMT-09-HPHT_graphs-3together.sh
+++ GMT-09-HPHT_graphs-3together.sh
@@ -10,28 +10,33 @@ gmt set FONT_ANNOT_PRIMARY=9p,Palatino-Roman,dimgray \
     MAP_GRID_PEN_PRIMARY=thinnest,dimgray \
     MAP_GRID_PEN_SECONDARY=thinnest,dimgray \
     FONT_LABEL=10p,Palatino-Roman,dimgray
+
 # Step-1. Generate a file
 ps=HPHTgraphs.ps
 #
 # HJORT graph
 gmt psxy -R-200/200/-7000/0 -JX15.2c/5 envH.txt \
-    -Bpxag100f10+l"Distance from trench (km)"\
+    -Bpxa50g50f10+l"Distance from trench (km)"\
     -Bpya2000gf+l"Depth (m)" \
     -Bsxg50 -Bsyg1000 -BWeSn \
+    --FONT_ANNOT_PRIMARY=11p,Helvetica,black \
+    --FONT_LABEL=12p,Helvetica,black \
+    --MAP_GRID_PEN_PRIMARY=thinnest,dimgray \
+    --MAP_GRID_PEN_SECONDARY=thinnest,dimgray \
 	-UBL/-5p/-35p -Glightgray -W0.5p -K > $ps
 gmt psxy -R -J -W1.0p -Ey+p0.2p stackH.txt -O -K >> $ps
 gmt psxy -R -J -W1.0p,red stackH.txt -O -K >> $ps
 # Step-17. Add test annotations
 echo "110 -5000 Pacific Plate" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,brown -O -K >> $ps
+    -Gwhite -F+jBL+f11p,brown -O -K >> $ps
 echo "-10 -1000 Hjort Trench" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,navyblue -O -K >> $ps
+    -Gwhite -F+jBL+f12p,navyblue -O -K >> $ps
 echo "90 -5000 Modelled median stacked" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "90 -5700 cross-section profiles" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "90 -6400 with error bars" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "-190 -6200 C" | gmt pstext -R -J -F+jBL+f18p,black \
     -Gfloralwhite -W0.5p -O -K >> $ps
 # Step-17. Arrow
@@ -41,25 +46,29 @@ EOF
 #
 # PUYSEGUR graph
 gmt psxy -R-200/200/-7000/1000 -JX15.2c/5 -Y6.5c envP.txt \
-    -Bpxag100f10+l"Distance from trench (km)"\
+    -Bpxa50g50f10+l"Distance from trench (km)"\
     -Bpya2000gf+l"Depth (m)" \
     -Bsxg50 -Bsyg1000 -BWeSn \
+    --FONT_ANNOT_PRIMARY=11p,Helvetica,black \
+    --FONT_LABEL=12p,Helvetica,black \
+    --MAP_GRID_PEN_PRIMARY=thinnest,dimgray \
+    --MAP_GRID_PEN_SECONDARY=thinnest,dimgray \
     -Glightgray -W0.5p -O -K >> $ps
 gmt psxy -R -J -W1.0p -Ey+p0.2p stackP.txt -O -K >> $ps
 gmt psxy -R -J -W1.0p,red stackP.txt -O -K >> $ps
 # Step-17. Add test annotations
 echo "-190 -1000 Modelled median stacked" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "-190 -1700 cross-section profiles" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "-190 -2400 with error bars" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "110 -6000 Pacific Plate" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,brown -O -K >> $ps
-echo "60 -500 Puysegur Ridge" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,brown -O -K >> $ps
+    -Gwhite -F+jBL+f11p,brown -O -K >> $ps
+echo "56 0 Puysegur Ridge" | gmt pstext -R -J \
+    -Gwhite -F+jBL+f11p,brown -O -K >> $ps
 echo "-50 0 Puysegur Trench" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,navyblue -O -K >> $ps
+    -Gwhite -F+jBL+f12p,navyblue -O -K >> $ps
 echo "-190 -6200 B" | gmt pstext -R -J -F+jBL+f18p,black \
     -Gfloralwhite -W0.5p -O -K >> $ps
 # Step-17. Arrow
@@ -69,25 +78,29 @@ EOF
 #
 # HIKURANGI graph
 gmt psxy -R-200/200/-4500/1000 -JX15.2c/5 -Y6.5c envHis.txt \
-    -Bpxag100f10+l"Distance from trench (km)"\
+    -Bpxa50g50f10+l"Distance from trench (km)"\
     -Bpya1000gf+l"Depth (m)" \
     -Bsxg50 -Bsyg1000 -BWeSn \
+    --FONT_ANNOT_PRIMARY=11p,Helvetica,black \
+    --FONT_LABEL=12p,Helvetica,black \
+    --MAP_GRID_PEN_PRIMARY=thinnest,dimgray \
+    --MAP_GRID_PEN_SECONDARY=thinnest,dimgray \
     -Glightgray -W0.5p -O -K >> $ps
 gmt psxy -R -J -W1.0p -Ey+p0.2p stackHis.txt -O -K >> $ps
 gmt psxy -R -J -W1.0p,red stackHis.txt -O -K >> $ps
 # Step-17. Add test annotations
 echo "90 -3000 Modelled median stacked " | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "90 -3500 cross-section profiles" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "90 -4000 with error bars" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,Times-Roman,red -O -K >> $ps
+    -Gwhite -F+jBL+f11p,Times-Roman,red -O -K >> $ps
 echo "110 -6000 Pacific Plate" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,brown -O -K >> $ps
+    -Gwhite -F+jBL+f11p,brown -O -K >> $ps
 echo "100 -1000 North Island" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,brown -O -K >> $ps
-echo "-20 500 Hikurangi Trench" | gmt pstext -R -J \
-    -Gwhite -F+jBL+f10p,navyblue -O -K >> $ps
+    -Gwhite -F+jBL+f11p,brown -O -K >> $ps
+echo "-40 500 Hikurangi Trench" | gmt pstext -R -J \
+    -Gwhite -F+jBL+f12p,navyblue -O -K >> $ps
 echo "-190 -3800 A" | gmt pstext -R -J -F+jBL+f18p,black \
     -Gfloralwhite -W0.5p -O -K >> $ps
 # Step-17. Arrow
